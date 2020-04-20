f44c3fb6fa094e6da94476da543a31db8e27605b
JimmyPhas <JimmyPhasouk@gmail.com>
2020-04-20 06:50:35 -0400

    restructured the layout of some of the pages and added some elements. somewhat got a working carousel in the displaymode

--- src/app/admin/team-management/team-management.component.ts
+++ src/app/admin/team-management/team-management.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamManagementComponent implements OnInit {
 
+  settings = {
+    columns: {
+      teamname: {
+        title: 'Team Name',
+      },
+      artName: {
+        title: 'ART Name',
+      },
+      POname: {
+        title: 'PO Name',
+      },
+      SMname: {
+        title: 'SM Name',
+      },
+      twaURL: {
+        title: 'URL of Team Working Agreement'
+      }
+    },
+    defaultStyle: false,
+    attr: {
+      class: 'table table-bordered'
+    }
+  };
+
+  data = [
+  {
+    teamname: "Sprintellectuals",
+    artName: "Choo Choo",
+    POname: "Smartha Knowledgeberry",
+    SMname: "Speedy McSprinterson",
+    twaURL: " "
+  }
+];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/display-mode/display-mode.component.ts
+++ src/app/display-mode/display-mode.component.ts
@@ -11,6 +11,32 @@ import { DataService } from '../models/data.service';
 export class DisplayModeComponent implements OnInit{
   teams$: Team[];
 
+  
+
+  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+    public pieChartData = [120, 150, 180, 90];
+    public pieChartType = 'pie';
+
+    public barChartOptions = {
+      scaleShowVerticalLines: false,
+      responsive: true,
+      maintainAspectRatio: false,
+  
+      layout: {
+        padding: {
+          top: 200
+        }
+      }
+  
+    };
+    public barChartLabels = ['2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartType = 'bar';
+    public barChartLegend = true;
+    public barChartData = [
+      {data: [59, 80, 81, 56, 55, 40], label: 'Series A'},
+      {data: [48, 40, 19, 86, 27, 90], label: 'Series B'}
+    ];
+
   constructor(private dataService: DataService) {
   }
 
