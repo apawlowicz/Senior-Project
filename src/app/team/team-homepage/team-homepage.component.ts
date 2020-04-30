f5572a22b324b85a906c0c40f084e3d983fda397
JimmyPhas <JimmyPhasouk@gmail.com>
2020-04-30 03:51:02 -0400

    changed the team-homepage component to use team data object

--- src/app/team/team-homepage/team-homepage.component.ts
+++ src/app/team/team-homepage/team-homepage.component.ts
@@ -22,19 +22,21 @@ export class TeamHomepageComponent implements OnInit {
   public PO: string;
   public RTE: string;
 
-  public teammembers = {
-    teamId: 8,
-    teamName: 'Sprintellectuals',
-    member: [ 
-      {userid: 47, username: 'Speedy McSpeederson', role: 'SM', teamName: 'Sprintellectuals', artName: 'Test Train'},
-      {userid: 48, username: 'Smartha Knowlegeberry', role: 'PO', teamName: 'Sprintellectuals', artName: 'Test Train'},
-      {userid: 49, username: 'Rick Grimes', role: 'TL', teamName: 'Sprintellectuals', artName: 'Test Train'},
-      {userid: 50, username: 'Carl Grimes', role: 'TM', teamName: 'Sprintellectuals', artName: 'Test Train'},
-      {userid: 51, username: 'Daryl Dixon', role: 'TM', teamName: 'Sprintellectuals', artName: 'Test Train'},
-      {userid: 52, username: 'Glenn Rhee', role: 'TM', teamName: 'Sprintellectuals', artName: 'Test Train'},
-      {userid: 53, username: 'Guywith Sillyhat', role: 'RTE', teamName: 'Sprintellectuals', artName: 'Test Train'},
-    ]
-  }
+  public Team: Team
+
+  // public teammembers = {
+  //   teamId: 8,
+  //   teamName: 'Sprintellectuals',
+  //   member: [ 
+  //     {userid: 47, username: 'Speedy McSpeederson', role: 'SM', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //     {userid: 48, username: 'Smartha Knowlegeberry', role: 'PO', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //     {userid: 49, username: 'Rick Grimes', role: 'TL', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //     {userid: 50, username: 'Carl Grimes', role: 'TM', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //     {userid: 51, username: 'Daryl Dixon', role: 'TM', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //     {userid: 52, username: 'Glenn Rhee', role: 'TM', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //     {userid: 53, username: 'Guywith Sillyhat', role: 'RTE', teamName: 'Sprintellectuals', artName: 'Test Train'},
+  //   ]
+  // }
 
   teams$: Team[];
   
@@ -85,17 +87,55 @@ export class TeamHomepageComponent implements OnInit {
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.Team = {
+      artId: 2, artName: 'Big Biz', teamId: 1, teamName: 'Sopranos', 
+      teamMembers: [ 
+        {userId: 1, username: 'Jeremy', roleId: 5, role: 'RTE', teamId: 1, teamName: 'Sopranos',artId: 2, artName: 'Big Biz'},
+        {userId: 2, username: 'Justin', roleId: 4, role: 'STE', teamId: 1, teamName: 'Sopranos',artId: 2, artName: 'Big Biz'},
+        {userId: 46, username: 'Danilo', roleId: 7, role: 'SM', teamId: 1, teamName: 'Sopranos', artId: 2, artName: 'Big Biz'},
+        {userId: 45, username: 'Augustus', roleId: 6, role: 'PO', teamId: 1, teamName: 'Sopranos', artId: 2, artName: 'Big Biz'},
+        {userId: 44, username: 'Kristin', roleId: 8, role: 'SU', teamId: 1, teamName: 'Sopranos', artId: 2, artName: 'Big Biz'},
+        {userId: 43, username: 'Andria', roleId: 8, role: 'SU', teamId: 1, teamName: 'Sopranos', artId: 2, artName: 'Big Biz'},
+        {userId: 41, username: 'Donovan',roleId: 8,  role: 'SU', teamId: 1, teamName: 'Sopranos', artId: 2, artName: 'Big Biz'},
+      ],
+      teamData: [
+        {type: 'line', labels: ['2007', '2008', '2009', '2010', '2011', '2012'], 
+          dataSet: [
+            {data: [59, 80, 81, 56, 55, 40], label: 'Series A'},
+            {data: [48, 40, 19, 86, 27, 90], label: 'Series B'}
+          ]
+        },
+        {type: 'line', labels: ['2007', '2008', '2009', '2010', '2011', '2012'],
+          dataSet: [
+            {data: [59, 80, 81, 56, 55, 40], label: 'Series A'},
+            {data: [48, 40, 19, 86, 27, 90], label: 'Series B'}
+          ]
+        },
+        {type: 'bar', labels: ['2007', '2008', '2009', '2010', '2011', '2012'],
+          dataSet: [
+            {data: [59, 80, 81, 56, 55, 40], label: 'Series A'},
+            {data: [48, 40, 19, 86, 27, 90], label: 'Series B'}
+          ]
+        },
+        {type: 'pie', labels: ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'],
+          dataSet: [
+            {data: [120, 150, 180, 90], label: ''}
+          ]
+        },
+      ]
+    }
 
-    for (let i in this.teammembers.member)
+
+    for (let i in this.Team.teamMembers)
     {
-      if (this.teammembers.member[i].role == 'SM') {
-        this.TSM = this.teammembers.member[i].username;
+      if (this.Team.teamMembers[i].role == 'SM') {
+        this.TSM = this.Team.teamMembers[i].username;
       }
-      else if (this.teammembers.member[i].role == 'PO') {
-        this.PO = this.teammembers.member[i].username;
+      else if (this.Team.teamMembers[i].role == 'PO') {
+        this.PO = this.Team.teamMembers[i].username;
       }
-      else if (this.teammembers.member[i].role == 'RTE') {
-        this.RTE = this.teammembers.member[i].username;
+      else if (this.Team.teamMembers[i].role == 'RTE') {
+        this.RTE = this.Team.teamMembers[i].username;
       }
     }
     
